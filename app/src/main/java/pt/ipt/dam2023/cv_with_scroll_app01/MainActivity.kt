a6bcd94fa9bd5b7dc60bf7d5cfcae9cfdf225abf
joaom <joaom@DESKTOP-PNCVF6D>
2023-10-06 15:33:11 +0100

    Initial commit

---
+++ app/src/main/java/pt/ipt/dam2023/cv_with_scroll_app01/MainActivity.kt
@@ -0,0 +1,11 @@
+package pt.ipt.dam2023.cv_with_scroll_app01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CV_with_scroll_app01"
+include(":app")
